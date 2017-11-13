dd6b1fe505d4cd7198aa0a4c45370e53a0381c26
admin <dangtinhtong@gmail.com>
2017-11-13 23:21:39 +0700

    First commit

--- Prog2.5/main.m
+++ Prog2.5/main.m
@@ -6,12 +6,24 @@
 //  Copyright © 2017 admin. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h> // This is a system file
+                                  // #import says to import or include the information from that file into the program, exactly as if the contents if the file were typed into  program at that point
+                                  // imported the file Foundation.h because it has imformation about other classes and functions that are used later in the program
+
 
 int main(int argc, const char * argv[]) {
+    
+    // Main is a special name that indicates precisely where the program is to begin excution
+    
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        
+        int sum, value1, value2;
+        value1=10;
+        value2=12;
+        sum=value1+value2;
+        NSLog(@"The sum of two number %i and %i is %i", value1, value2, sum);
     }
     return 0;
 }
---
+++ Readme.md
@@ -0,0 +1,3 @@
+* This is a small program.
+* The program adds two numbers;
+* int value1 and value2 , int sum=value1+value2.
